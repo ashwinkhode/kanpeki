b532a8e332043ec5f217fe68b4a5fa84680aef97
Ashwin Khode <ashwin.khode1@gmail.com>
2021-08-05 14:14:06 +0530

    fix: ws port for hmr

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     watch: {
-      usePolling: true
-    }
-  }
-})
+      usePolling: true,
+    },
+    hmr: {
+      port: 443,
+    },
+  },
+});
